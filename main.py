d26ce00c7688b5af63ca98a7b4e1076f35f8d100
Cyber <cybr1998@gmail.com>
2018-11-05 17:10:54 +0530

    Add feature to update an entry
    Any of the fields can be updated:
     - Year of truncation
     - Seasons/Episodes watched
     - Rating
     - Genre

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def cli():
     return None
 
 
-@cli.command('update_existing')
+@cli.command('update')
 def update_existing_show():
 
     spreadsheet = _choose_spreadsheet()
@@ -29,7 +29,118 @@ def update_existing_show():
     service = build('sheets', 'v4', http=creds.authorize(Http()))
 
 
-@cli.command('update_new')
+
+    result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
+                                                 range=RANGE_NAME).execute()
+    rows = result.get('values', [])
+    if not rows:
+        print('No data found.')
+    else:
+
+        print(
+            '{:<4} | {:<40} | {:<17} | {:<20} | {:<26} | {:<8} | {:<30}'
+                .format(
+                'ID',
+                'Name',
+                'Year of release',
+                'Year of truncation',
+                'Seasons/Episodes watched',
+                'Rating',
+                'Genre'
+            )
+        )
+        for row in rows:
+            print(
+                '{:<4} | {:<40} | {:<17} | {:<20} | {:<26} | {:<8} | {:<30}'
+                    .format(
+                    row[0],
+                    row[1],
+                    row[2],
+                    row[3],
+                    row[4],
+                    row[5] if row[5] else '',
+                    row[6] if row[6] else ''
+                )
+            )
+
+    id_ = int(input('Enter the ID of the show: '))
+    RANGE_NAME = 'Sheet1!D{}:G'.format(id_ + 1)
+
+    termination_year = input('Year it ended: ')
+    options = {
+        1: 'Yet to watch',
+        2: 'Watching',
+        3: 'Finished watching :(',
+    }
+    click.echo('Options: ')
+    for option in options.items():
+        print("Type", option[0], "to set status as:", option[1])
+
+    status = int(input('Status: '))
+    if status not in options.keys():
+        raise ValueError('Invalid option selected')
+
+    if options[status] == options[2]:
+        season = input("Which season are you watching? ")
+        episode = input("Which episode are you watching? ")
+        final_status = 'S{}E{}'.format(season, episode)
+    else:
+        final_status = options[status]
+
+    rating = input('What would you rate it? ')
+    genre = input('Genre of the series? ')
+
+    ty, st, rt, gn = 'N/A', 'N/A', 'N/A', 'N/A'
+
+    for row in rows:
+        if int(row[0]) == id_:
+            ty = row[3]
+            st = row[4]
+            rt = row[5]
+            gn = row[6
+            ]
+    values_to_update = []
+    print(ty, st, rt, gn)
+    if termination_year:
+        values_to_update.append(termination_year)
+    else:
+        values_to_update.append(ty)
+
+
+    if final_status:
+        values_to_update.append(final_status)
+    else:
+        values_to_update.append(st)
+
+
+    if rating:
+        values_to_update.append(rating)
+    else:
+        values_to_update.append(rt)
+
+
+    if genre:
+        values_to_update.append(genre)
+    else:
+        values_to_update.append(gn)
+
+
+    if not values_to_update:
+        print('Nothing to update')
+        return
+
+    values = [values_to_update]
+
+    Body = {
+        'values': values
+    }
+
+    result = service.spreadsheets().values().update(
+        spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME,
+        valueInputOption='RAW', body=Body).execute()
+
+
+@cli.command('new')
 def update_new_show():
 
     spreadsheet = _choose_spreadsheet()
